b099421a6077d795aaf7cf3c16f324cd078b93e1
Scout Finch <scoutfinch@Scout-Finch.local>
2025-09-29 15:00:20 +0900

    Build a /docs para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/galeria-2d/',   // ← exactamente el nombre del repo
+  base: '/galeria-2d/',     // ← nombre EXACTO del repo
+  build: { outDir: 'docs' },// ← Vite generará la web en /docs
   plugins: [react()],
 })
